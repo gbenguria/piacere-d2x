9001c6ed2cc3b5f72bd32db88ccf3b1d43a4abeb
Gorka Benguria <gorka.benguria@tecnalia.com>
2023-03-31 00:42:17 +0200

    version from generator

---
+++ src/test/java/eu/piacere/d2x/server/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.piacere.d2x.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
