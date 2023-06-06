0992c8a969e123ed0e7dc62f3913d6dbf8162d03
Gorka Benguria <gorka.benguria@tecnalia.com>
2023-06-07 00:09:11 +0200

    enables domlx to doml

--- src/main/java/eu/piacere/d2x/server/service/Domlx2domlService.java
+++ src/main/java/eu/piacere/d2x/server/service/Domlx2domlService.java
@@ -2,6 +2,20 @@ package eu.piacere.d2x.server.service;
 
 import eu.piacere.d2x.server.api.Doml2domlxApiDelegate;
 import eu.piacere.d2x.server.api.Domlx2domlApiDelegate;
+import eu.piacere.doml.DomlStandaloneSetup;
+import eu.piacere.doml.doml.application.ApplicationPackage;
+import eu.piacere.doml.doml.commons.CommonsPackage;
+import eu.piacere.doml.doml.commons.DOMLModel;
+import eu.piacere.doml.doml.concrete.ConcretePackage;
+import eu.piacere.doml.doml.infrastructure.InfrastructurePackage;
+import eu.piacere.doml.doml.optimization.OptimizationPackage;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +23,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.util.UUID;
 
 @Service
 public class Domlx2domlService implements Domlx2domlApiDelegate {
@@ -22,6 +40,103 @@ public class Domlx2domlService implements Domlx2domlApiDelegate {
 
     @Override
     public ResponseEntity<String> domlx2domlPost(String body) {
-        return new ResponseEntity<>("not implemented", HttpStatus.NOT_IMPLEMENTED);
+        String sourceExt = "domlx";
+        String targetExt = "doml";
+
+        DomlStandaloneSetup.doSetup();
+        if (!EPackage.Registry.INSTANCE.containsKey(CommonsPackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(CommonsPackage.eNS_URI, CommonsPackage.eINSTANCE);
+        }
+        if (!EPackage.Registry.INSTANCE.containsKey(ApplicationPackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(ApplicationPackage.eNS_URI, ApplicationPackage.eINSTANCE);
+        }
+        if (!EPackage.Registry.INSTANCE.containsKey(InfrastructurePackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(InfrastructurePackage.eNS_URI, InfrastructurePackage.eINSTANCE);
+        }
+        if (!EPackage.Registry.INSTANCE.containsKey(ConcretePackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(ConcretePackage.eNS_URI, ConcretePackage.eINSTANCE);
+        }
+        if (!EPackage.Registry.INSTANCE.containsKey(OptimizationPackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(OptimizationPackage.eNS_URI, OptimizationPackage.eINSTANCE);
+        }
+        if (!Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().containsKey("domlx")) {
+            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("domlx", new XMIResourceFactoryImpl());
+        }
+
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        // https://www.eclipse.org/forums/index.php/t/134790/
+        // generate fake URI
+        String sourceURIFakeString = getUniqueFileName(tmpDir, sourceExt);
+
+        // create inputStream from body
+        InputStream inputStream  = new ByteArrayInputStream(body.getBytes());
+
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        Resource inputResource = resourceSet.createResource(URI.createFileURI(sourceURIFakeString));
+
+        try{
+            inputResource.load(inputStream, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error while loading domlx into ecore resource", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        logger.info("domlx loaded into ecore resource");
+
+        // print inputResource errors
+        for (Resource.Diagnostic domlError : inputResource.getErrors()) {
+            logger.error("Domlx error: " + domlError.getMessage());
+        }
+
+        String targetURIFakeString = getUniqueFileName(tmpDir, targetExt);
+
+        Resource targetResource = resourceSet.createResource(URI.createFileURI(targetURIFakeString));
+        try {
+            DOMLModel model = (DOMLModel) inputResource.getContents().get(0);
+            targetResource.getContents().add(EcoreUtil.copy(model));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error while creating doml from ecore resource", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        logger.info("doml created from ecore resource");
+
+        // create outputStream from targetResource https://stackoverflow.com/a/1022434
+        OutputStream outputStream = new OutputStream() {
+            private StringBuilder string = new StringBuilder();
+
+            @Override
+            public void write(int b) throws IOException {
+                this.string.append((char) b );
+            }
+
+            //Netbeans IDE automatically overrides this toString()
+            public String toString() {
+                return this.string.toString();
+            }
+        };
+
+        try {
+            targetResource.save(outputStream,null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // get stacktrace as string https://stackoverflow.com/a/1149712
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            return new ResponseEntity<>("Error while loading doml into string: \n" + e.getMessage() + "\nStackTrace:\n" + sw, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        logger.info("doml loaded into string");
+        String doml = outputStream.toString();
+
+        // return doml
+        return new ResponseEntity<>(doml, HttpStatus.CREATED);
     }
+
+    public static String getUniqueFileName(String directory, String extension) {
+        String fileName = MessageFormat.format("{0}.{1}", UUID.randomUUID(), extension.trim());
+        return Paths.get(directory, fileName).toString();
+    }
+
 }
